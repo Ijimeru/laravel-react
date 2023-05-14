e8bcb0af9ab7a08452f7e381b77c107a8a60f37f
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-05-14 15:16:17 +0700

    commit ke-2

---
+++ app/Jobs/SendEmailVerification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+class SendEmailVerification implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(User $user): void
+    {
+        event(new Registered($user));
+    }
+}

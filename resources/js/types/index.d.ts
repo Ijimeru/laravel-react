5f5cd5699aebe766985ff0933ef837602bcf6328
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-06-14 16:57:09 +0700

    Membuat tipe data kategori membuat tipe data post membuat tipe data ziggy

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -11,10 +11,30 @@ export interface ChirpType {
     user: { name: string; id: number };
     updated_at: string;
 }
+
+export interface CategoryType {
+    id: number;
+    name: string;
+    posts: PostType[];
+}
+
+export interface PostType {
+    id: number;
+    categories: CategoryType[];
+    image: string;
+    title: string;
+    slug: string;
+    excerpt: string;
+    body: string;
+    published_at: string;
+    created_at: string;
+    updated_at: string;
+}
 export type PageProps<
     T extends Record<string, unknown> = Record<string, unknown>
 > = T & {
     auth: {
         user: User;
     };
+    ziggy: { query: { page: string } };
 };

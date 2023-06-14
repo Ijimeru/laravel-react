f7cd2631ddfaf0bd0c764af6640c9bb339457ab2
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-06-14 16:53:35 +0700

    Membuat agar footer menjadi sebuah komponen

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
+import MainFooter from "@/Components/MainFooter";
 import Navbar from "@/Components/Navbar";
-import { HTMLAttributes, useEffect, useState } from "react";
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import Navbarcoba from "@/Components/Navbarcoba";
+import { HTMLAttributes, useEffect } from "react";
 
 export default function Main({ children }: HTMLAttributes<HTMLDivElement>) {
     useEffect(() => {
@@ -15,26 +16,10 @@ export default function Main({ children }: HTMLAttributes<HTMLDivElement>) {
             className={`flex justify-between flex-col bg-secondary dark:bg-secondaryDark text-primary dark:text-primaryDark scroll-smooth`}
         >
             <Navbar />
-            <div className="min-h-[calc(100vh-72px-144px)]">{children}</div>
-            <footer className="h-36 dark:bg-gradient-to-t dark:from-secondaryButtonDark dark:to-secondaryDark bg-gradient-to-t from-primaryDark to-secondary text-center">
-                <p className="py-[15px]">
-                    &copy; 2023 HIMATEKIA. All rights reserved.
-                </p>
-                <div className="flex justify-center items-center text-lg gap-x-3">
-                    <a href="#">
-                        <FaFacebook className="transition-all hover:-translate-y-[5px]" />
-                    </a>
-                    <a href="#">
-                        <FaTwitter className="transition-all  hover:-translate-y-[5px]" />
-                    </a>
-                    <a href="#">
-                        <FaInstagram className="transition-all  hover:-translate-y-[5px]" />
-                    </a>
-                    <a href="#">
-                        <FaYoutube className="transition-all  hover:-translate-y-[5px]" />
-                    </a>
-                </div>
-            </footer>
+            <div className="min-h-[calc(100vh-72px-410px)]">{children}</div>
+
+            <br />
+            <MainFooter />
         </div>
     );
 }

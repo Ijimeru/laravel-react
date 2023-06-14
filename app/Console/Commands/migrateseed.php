94323dd330e2fdb9cf1e49a1a04e23df97eea5a5
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-06-14 16:57:42 +0700

    membuat command untuk migrasi secara cepat tanpa eksekusi beberapa command

---
+++ app/Console/Commands/migrateseed.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class migrateseed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:migrateseed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrasi+seeding full';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $this->call('migrate:fresh',[
+            '--seed' => true,
+        ]);
+        $this->call('db:seed',[
+            '--class' => 'CategorySeeder',
+        ]);
+        $this->call('db:seed',[
+            '--class' => 'PostSeeder',
+        ]);
+    }
+}

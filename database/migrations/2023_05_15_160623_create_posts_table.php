c88ecb6255297ea88a65dac68a9bd3fa20a7b38d
MHabibiWasi <muhammad.120280027@student.itera.ac.id>
2023-06-14 16:47:30 +0700

    Menghapus kolom category id pada migrasi post

--- database/migrations/2023_05_15_160623_create_posts_table.php
+++ database/migrations/2023_05_15_160623_create_posts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id');
             $table->foreignId('user_id');
             $table->string('image')->nullable();
             $table->string('title');
